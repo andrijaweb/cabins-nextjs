be29c22c2a03a02095553c738ca9531b8a8a6b17
andrijaweb <andrijawebd@gmail.com>
2024-08-09 22:06:59 +0200

    Improved homepage, added style to body

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+        className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
       >
         <Header />
 
