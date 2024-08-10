41acb5fc8ef41f5046aa813a31f691be85b7bc22
andrijaweb <andrijawebd@gmail.com>
2024-08-10 21:22:11 +0200

    Added error boundaries and not-found errors

--- app/_lib/data-service.ts
+++ app/_lib/data-service.ts
@@ -1,6 +1,7 @@
 import { eachDayOfInterval } from "date-fns";
 import { supabase } from "./supabase";
 import type { Database } from "./database.types";
+import { notFound } from "next/navigation";
 
 /////////////
 // GET
@@ -19,6 +20,7 @@ export async function getCabin(
 
   if (error) {
     console.error(error);
+    notFound();
   }
 
   return data;
